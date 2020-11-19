544f9a0264bb52bb60342e02061b152d608d3a7d
Adwitiya Trivedi <adw.tri2012@gmail.com>
2020-11-20 00:14:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <h1>
+          Hi I am a react app, and I will become a bots-builder.
+        </h1>
+        <h2>I love lazy loading so bear with me.</h2>
     </div>
   );
 }
